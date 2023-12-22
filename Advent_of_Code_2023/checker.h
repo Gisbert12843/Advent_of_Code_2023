1f3a298bf03e076c888f6a561067cec3eb7e737c
Kai Oestreich <51728041+Gisbert12843@users.noreply.github.com>
2023-12-22 13:13:16 +0100

    added input logic

--- Advent_of_Code_2023/checker.h
+++ Advent_of_Code_2023/checker.h
@@ -6,6 +6,6 @@
 #include <utility>
 
 
-inline std::unordered_map<std::string, std::tuple<std::string, int>> pattern_map = {};
+static std::unordered_map<std::string, std::tuple<std::string, int>> pattern_map = {};
 
 int checker(std::string current_pump_status_string, std::string current_number_string, int current_collision_count = 0);
--- Advent_of_Code_2023/main.cpp
+++ Advent_of_Code_2023/main.cpp
@@ -1,11 +1,49 @@
 #include "checker.h"
 
-static std::string pump_status_string = "..??..??..", number_string = "11";
-
 
 int main()
 {
-	std::cout << checker("????.######..#####.", "165");
+	std::vector<std::tuple<std::string, std::string>> input_vector = {};
+	std::string current = "";
 
+	int line = 0;
+	while (std::getline(std::cin, current))
+	{
+		if (current.empty())
+			break;
+
+		int mode = 0;
+		std::string first = "", second = "";
+
+		for (auto it : current)
+		{
+			if (it == ' ')
+			{
+				mode = 1;
+				continue;
+			}
+			else if (it == ',')
+				continue;
+
+			if (mode == 0)
+			{
+				first.push_back(it);
+			}
+			else
+				second.push_back(it);
+		}
+		input_vector.push_back(std::make_tuple(first, second));
+		line++;
+	}
+
+
+	int sum = 0;
+
+	for (auto it : input_vector)
+	{
+		std::string first = std::get<0>(it), second = std::get<1>(it);
+		sum += checker(first, second);
+	}
+	std::cout << sum;
 }
 
